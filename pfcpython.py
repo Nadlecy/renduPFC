50e5996078e8c3d2062a7e78284dba794dbf7e45
Nadlecy <bruno.cleymand@gmail.com>
2022-11-09 14:34:03 +0100

    et là ça matche ?

---
+++ pfcpython.py
@@ -0,0 +1,10 @@
+# Début
+
+# On admet une fonction random() qui retourne un integer aléatoire entre 0 et 2 compris
+# On admet une fonction input qui enregistre l'entree d'un joueur sous forme de str
+
+# On definit une fonction pfc qui ne prend aucun argument
+    # On definit la variable comPoints à 0
+    # On definit la variable playerPoints à 0
+    # tant que playerPoints est inférieure à 3 et que comPoints est inférieure à 3
+        # 
