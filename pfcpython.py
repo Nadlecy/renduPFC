659d648148b7f017bfcbef04f36875524720368a
Nadlecy <bruno.cleymand@gmail.com>
2022-11-09 15:19:54 +0100

    add

--- pfcpython.py
+++ pfcpython.py
@@ -1,10 +1,24 @@
 # Début
 
 # On admet une fonction random() qui retourne un integer aléatoire entre 0 et 2 compris
-# On admet une fonction input qui enregistre l'entree d'un joueur sous forme de str
+# On admet une fonction input() qui enregistre l'entree d'un joueur sous forme de str
 
 # On definit une fonction pfc qui ne prend aucun argument
+"""INITIALISATION"""
+    # On definit une liste choice qui represente Pierre, Feuille et Ciseaux respectivement sous forme d'entiers
     # On definit la variable comPoints à 0
     # On definit la variable playerPoints à 0
-    # tant que playerPoints est inférieure à 3 et que comPoints est inférieure à 3
-        # 
+"""DEBUT BOUCLE"""
+    # Tant que playerPoints est inférieure à 3 et que comPoints est inférieure à 3
+        # On definit une variable validAnswer à True
+        # On definit une variable answer contenant la réponse du joueur, acquise grâce à input()
+"""ANALYSE REPONSE"""
+        # Si answer contient à "P" ou "Pierre"
+            # On definit 
+        # Sinon Si answer contient "F" ou "Feuille"
+            #
+        # Sinon Si answer contient "C" ou "Ciseaux"
+            #
+        # Sinon
+            # Afficher un message de réponse invalide
+            # Definir validAnswer à False
